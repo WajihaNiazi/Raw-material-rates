a2f75cf55c581bd1e2aaf9b71e19c296816e74b5
WajihaNiazi <wajihaniazi204@gmail.com>
2021-03-19 13:26:48 +0430

    some change

---
+++ screens/navlink.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import {
+    View,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    StyleSheet,
+    Image
+} from 'react-native';
+import Colors from '../utils/colors';
+import * as SQLite from "expo-sqlite";
+import { Feather } from "@expo/vector-icons";
+// const db = SQLite.openDatabase('productRate.db')
+
+export default function Navlink({navigation}){
+    return (
+      <View>
+            <Text>link</Text>
+      </View>
+    )
+
+}
+const styles =StyleSheet.create({
+})
